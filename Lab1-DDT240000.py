2d86a9377b01233b1daaa9d33af4b21c5c8d40fe
David Truong <ddt240000@utdallas.edu>
2026-09-09 20:28:00 -0500

    I like pigeons

---
+++ Lab1-DDT240000.py
@@ -0,0 +1,14 @@
+Case1= "CARTER, DEMARCUS | m | 36 | 2018-03-22 | 412 larkmoor lane | beat 352 | OPEN"
+Case2= "reyes, miguel|M|32|2018-05-17| 3308 DELAFORD STREET |Beat 347|OPEN"
+Case3= "OKAFOR, SAMUEL J | M | 55 | 2018-07-09 | 1519 westhollow avenue | beat 341 | OPEN"
+Case4= "Nguyen, Daniel | m | 17 | 2018-04-03 | 2846 QUAIL RIDGE ROAD | beat 537 | OPEN"
+Case5= "BOOKER, TERRENCE | M | 41 | 2018-11-08 | 907 n. calloway drive | Beat 442 | OPEN"
+Fields1= Case1.strip().split(" | ")
+Fields2= Case2.strip().split(" | ")
+Fields3= Case3.strip().split(" | ")
+Fields4= Case4.strip().split("|")
+Fields5= Case5.strip().split("|")
+
+
+
+print(Fields2)
