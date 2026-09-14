ed8d8cea6f4c1b521e287ab5eed79d49089bfd17
Utsab Panthi <uxp230006@utdallas.edu>
2026-09-14 01:21:56 -0500

    My Part For Week 2

--- wk2-intake.py
+++ wk2-intake.py
@@ -1,18 +1,17 @@
 #Nguyen, Daniel | m | 17 | 2018-04-03 | 2846 QUAIL RIDGE ROAD | beat 537 |Open
-record = "Nguyen, Daniel | m | 17 | 2018-04-03 | 2846 QUAIL RIDGE ROAD | beat 537 | Open"
-fields = record.strip().split(" | ")
-name = fields[0].title()
-sex = fields[1].upper()
-age = int(fields[2])
-date = fields[3]
-year = int(date[0:4])
-addr = fields[4].title() # "412 Larkmoor Lane"
-status = fields[5].upper()
-years_unsolved = 2026 - year
-
-CASE = 'CASE'
-AGE = 'AGE'
-YEARS_UNSOLVED = 'YEARS UNSOLVED'
-
-print(f"{'CASE':<30}{'AGE':>5}{'YEARS_UNSOLVED':>17}")
-print(f"{name} ({sex}){'':<13}{age:>3}{years_unsolved:>17}")
+record3 = "Nguyen, Daniel | m | 17 | 2018-04-03 | 2846 QUAIL RIDGE ROAD | beat 537 | Open"
+fields3 = record3.strip().split(" | ")
+name3 = fields3[0].title()
+sex3 = fields3[1].upper()
+age3 = int(fields3[2])
+date3 = fields3[3]
+year3 = int(date3[0:4])
+addr3 = fields3[4].title()
+status3 = fields3[5].upper()
+years_unsolved3 = 2026 - year3
+CASE3 = 'CASE'
+AGE3 = 'AGE'
+YEARS_UNSOLVED3 = 'YEARS_UNSOLVED'
+print(f"{CASE3:<30}{AGE3:>5}{YEARS_UNSOLVED3:>17}")
+print(f"{name3} ({sex3}){'':<13}{age3:>3}{years_unsolved3:>17}")
+print()
